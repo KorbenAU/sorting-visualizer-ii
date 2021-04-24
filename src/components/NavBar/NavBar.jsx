ad8045a33e7e70fbbd360076c7872dcc08afd4e0
Korben <gaoyuan.hello@gmail.com>
2021-04-24 23:15:44 +1000

    svg stroke width increase

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,15 +10,15 @@ const NavBar = ({ navConfig }) => {
                         <div className="flex flex-shrink-0 flex-row items-center">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
-                                className="h-6 w-6"
+                                className="h-8 w-8"
                                 fill="none"
                                 viewBox="0 0 24 24"
                                 stroke="currentColor"
                             >
                                 <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="3"
                                     d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"
                                 />
                             </svg>
